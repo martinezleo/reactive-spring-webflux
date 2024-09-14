1dedf69960cb64d3472fd0de7f7c8704abff5ff4
Leonardo Martinez <martinezleo1460@gmail.com>
2024-09-14 13:55:22 -0400

    fixed controller structure

--- movies-info-service/src/main/java/com/reactivespring/MoviesInfoServiceApplication.java
+++ movies-info-service/src/main/java/com/reactivespring/MoviesInfoServiceApplication.java
@@ -1,4 +1,4 @@
-package com.reactivespring.movies_info_service;
+package com.reactivespring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
